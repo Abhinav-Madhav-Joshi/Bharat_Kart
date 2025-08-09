7fac51cd23f4740e341edcf1f3624f58f20d7d7c
Abhinav <amj16122004@gmail.com>
2025-08-09 20:30:21 +0530

    Sample complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Logo from './assets/Logo.jpg'
+import Mishri from './assets/Mishri.jpeg'
+import Honey from './assets/Honey.jpeg'
+import Chivda from './assets/Chivda.jpeg'
+import Sattu from './assets/Sattu.jpeg'
+import Pickle from './assets/Pickle.jpeg'
+import Ghugni from './assets/Ghugni.jpeg'
+import Vijay from './assets/Vijay.jpg'
 import './App.css'
 
 function Home() {
   return (
     <>
       <h1 className='Title'>Bharat Kart</h1>
-      <body>
-        
-      </body>
+      <main className='main'>
+        <div className='Card'>
+          <img src={Mishri} className='images'/>
+          <span className='product'>Dhaga-Mishri <br /> &#8377;125 (200gm)</span>
+        </div>
+        <div className='Card'>
+          <img src={Honey} className='images'/>
+          <span className='product'>Pure Honey <br /> &#8377;250 (1L)</span>
+        </div>
+        <div className='Card'>
+          <img src={Chivda} className='images'/>
+          <span className='product'>Special Chivda <br /> &#8377;80 (150gm)</span>
+        </div>
+        <div className='Card'>
+          <img src={Sattu} className='images'/>
+          <span className='product'>Pure Sattu <br /> &#8377;300 (1kg)</span>
+        </div>
+        <div className='Card'>
+          <img src={Ghugni} className='images'/>
+          <span className='product'>Chana Ghugni <br /> &#8377;150 (200gm)</span>
+        </div>
+        <div className='Card'>
+          <img src={Pickle} className='images'/>
+          <span className='product'>Mango Pickle (Aam Aachar) <br /> &#8377;250 (1kg)</span>
+        </div>
+      </main>
     </>
   )
 }
 
 function About() {
   return <>
+    <img src={Vijay} className='Founder'/>
     <p className="About">
-      The founder of the company is legendary Mr. Vijay Anand, who build this company with a thought to build a brand which people can rely on and which is build on trust and the moto of purity.
+      Our story begins with the legendary Mr. Vijay Anand — a visionary who set out to create more than just a company. His mission? To craft a brand people could trust, built on a foundation of purity and reliability… with a sprinkle of excellence in every product.
     </p>
   </>
 }
@@ -25,7 +55,7 @@ function About() {
 
 function App() {
   return (
-    <>
+    <div style={{display:'flex',flexDirection:'column',minHeight:'100vh'}}>
       <nav className='Nav'>
         <div>
           <img src={Logo} className='logo'/>
@@ -35,12 +65,20 @@ function App() {
           <Link to="/about" className='link'>About</Link>
         </div>
       </nav>
-
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+
+      <div style={{flex:'1'}}>
+
+      </div>
+
+      <footer className='footer'>
+        <span style={{marginLeft:'1em',color:'black'}}> &copy; All Rights Reserved </span>
+      </footer>
+    </div>
   )
 }
 
